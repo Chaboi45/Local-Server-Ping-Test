8efc8aca42677215315d9dcf93a71086b1c7f9b4
Chaboi3000 <29741391+Chaboi45@users.noreply.github.com>
2020-01-17 16:35:06 +0900

    Update script.js
    Reload on enter

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function Enter(){
+  window.addEventListener("keydown", function (event) {
+          if (event.defaultPrevented) {
+            return; // Do nothing if event already handled
+          }
+
+          switch (event.code) {
+            case "Enter":
+            
+              // Handle "back"
+              location.reload(true);
+              break;
+            
+          }
+
+          
+
+          // Consume the event so it doesn't get handled twice
+          event.preventDefault();
+        }, true);
+}
+
 function ping(host, port, pong) {
 
   var started = new Date().getTime();
@@ -10,7 +32,7 @@ function ping(host, port, pong) {
   var http = new XMLHttpRequest();
 
   http.open("GET", "http://" + host + ":" + port, /*async*/true);
-  http.onreadystatechange = function() {
+  http.onreadystatechange = function () {
     if (http.readyState == 4) {
       var ended = new Date().getTime();
 
@@ -23,76 +45,80 @@ function ping(host, port, pong) {
   };
   try {
     http.send(null);
-  } catch(exception) {
+  } catch (exception) {
     // this is expected
   }
 
 }
-function flicker(isTrue){
-  if (isTrue == true){
+function flicker(isTrue) {
+  if (isTrue == true) {
     document.getElementById("flicker").innerHTML = ">_|";
-    isTrue=!isTrue
+    isTrue = !isTrue
   }
-  else if (isTrue==false){
+  else if (isTrue == false) {
     document.getElementById("flicker").innerHTML = ">_";
-    isTrue=!isTrue
+    isTrue = !isTrue
   }
 }
-function time(){
- var today = new Date();
+function time() {
+  var today = new Date();
   var hourNow = today.getHours();
   var minuteNow = today.getMinutes();
   var secondNow = today.getSeconds();
   var milNow = today.getMilliseconds();
-  
+
   document.getElementById("time").innerHTML = "Last login at: " + hourNow + ':' + minuteNow + ':' + secondNow;
 }
 time()
-ping("localhost", "3000", function(m){ document.getElementById("3000").innerHTML = "~guest$ Localhost:3000: "+m+" miliseconds"}) 
-ping("localhost", "9292", function(m){ document.getElementById("9292").innerHTML = "~guest$ Localhost:9292: "+m+" miliseconds"}) 
-ping("localhost", "", function(m){ document.getElementById("localhost").innerHTML = "~guest$ Localhost: "+m+" miliseconds"}) 
+ping("localhost", "3000", function (m) { document.getElementById("3000").innerHTML = "~guest$ Localhost:3000: " + m + " miliseconds" })
+ping("localhost", "9292", function (m) { document.getElementById("9292").innerHTML = "~guest$ Localhost:9292: " + m + " miliseconds" })
+ping("localhost", "", function (m) { document.getElementById("localhost").innerHTML = "~guest$ Localhost: " + m + " miliseconds" })
 
 //var isTrue=true
 //var intervalID = window.setInterval(flicker(isTrue), 1000);
-var text = [">_ |",">_ s",">_ su",">_ sud",">_ sudo",">_ sudo ",">_ sudo k",">_ sudo ki",">_ sudo kil",">_ sudo kill",">_ sudo kill ",">_ sudo kill ",">_ sudo kill a",">_ sudo kill al",">_ sudo kill all","","~guest$ sudo kill all"];
+var text = [">_ |", ">_ s", ">_ su", ">_ sud", ">_ sudo", ">_ sudo ", ">_ sudo k", ">_ sudo ki", ">_ sudo kil", ">_ sudo kill", ">_ sudo kill ", ">_ sudo kill ", ">_ sudo kill a", ">_ sudo kill al", ">_ sudo kill all", "", "~guest$ sudo kill all"];
 var counter = 0;
 var elem = document.getElementById("flicker");
-var inst = setInterval(change, getRandomInt(50,250));
+var inst = setInterval(change, getRandomInt(50, 250));
 
 function change() {
   elem.innerHTML = text[counter];
   counter++;
   if (counter >= text.length) {
     counter = 0;
-    clearInterval(inst); 
-    document.getElementById("abort").innerHTML="Successfully killed "+getRandomInt(5, 230)+" tasks"
-    var text1 = [">_ |",">_ M",">_ Ma",">_ Mad",">_ Made",">_ Made ",">_ Made b",">_ Made by",">_ Made by ",">_ Made by @",">_ Made by @C",">_ Made by @Ch",">_ Made by @Cha",">_ Made by @Chab",">_ Made by @Chabo",">_ Made by @Chaboi",">_ Made by @Chaboi4",">_ Made by @Chaboi45"];
-  var counter1 = 0;
-  var elem1 = document.getElementById("flicker2");
-  var inst1 = setInterval(change1, 120);
+    clearInterval(inst);
+    document.getElementById("abort").innerHTML = "Successfully killed " + getRandomInt(5, 230) + " tasks"
+    var text1 = [">_ |", ">_ M", ">_ Ma", ">_ Mad", ">_ Made", ">_ Made ", ">_ Made b", ">_ Made by", ">_ Made by ", ">_ Made by @", ">_ Made by @C", ">_ Made by @Ch", ">_ Made by @Cha", ">_ Made by @Chab", ">_ Made by @Chabo", ">_ Made by @Chaboi", ">_ Made by @Chaboi4", ">_ Made by @Chaboi45"];
+    var counter1 = 0;
+    var elem1 = document.getElementById("flicker2");
+    var inst1 = setInterval(change1, 75);
 
-  function change1() {
-    elem1.innerHTML = text1[counter1];
-    counter1++;
-    if (counter1 >= text1.length) {
-      counter1 = 0;
-    clearInterval(inst1); 
-    var text2 = [">_ Made by @Chaboi45",">_ Made by @Chaboi45|"];
-  var counter2 = 0;
-  var elem2 = document.getElementById("flicker2");
-  var inst2 = setInterval(change2, 500);
+    function change1() {
+      elem1.innerHTML = text1[counter1];
+      counter1++;
+      if (counter1 >= text1.length) {
+        counter1 = 0;
+        clearInterval(inst1);
+        var text2 = [">_ Made by @Chaboi45", ">_ Made by @Chaboi45|"];
+        var counter2 = 0;
+        var elem2 = document.getElementById("flicker2");
+        var inst2 = setInterval(change2, 500);
+        setInterval(Enter,1);
 
-  function change2() {
-    elem2.innerHTML = text2[counter2];
-    counter2++;
-    if (counter2 >= text2.length) {
-      counter2 = 0;
-    
-    // uncomment this if you want to stop refreshing after one cycle
+        
+
+        function change2() {
+          elem2.innerHTML = text2[counter2];
+          counter2++;
+          if (counter2 >= text2.length) {
+            counter2 = 0;
+
+
+            // uncomment this if you want to stop refreshing after one cycle
+          }
+        }
+      }
     }
-  }
-    }
-  }
 
     // uncomment this if you want to stop refreshing after one cycle
   }
